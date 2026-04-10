e5c3eb09511545fea6d08c2bd3ea43be1d2a2e27
Elias <elias@falchus.com>
2026-04-10 22:43:19 +0200

    Update WrappedPacketInFlying.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/flying/WrappedPacketInFlying.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/flying/WrappedPacketInFlying.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.flying;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInFlying extends PacketFlyingWrapper {
 
-    WrappedPacketInFlying(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInFlying",
-                        networkProtocolGame + "PacketPlayInFlying"
-                )
-        );
-    }
+	WrappedPacketInFlying(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInFlying",
+				networkProtocolGame + "PacketPlayInFlying"
+			)
+		);
+	}
 }
