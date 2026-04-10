1cf7e15b72d85412c8e581e83988e2a291fafbf3
Elias <elias@falchus.com>
2026-04-10 22:38:51 +0200

    Update WrappedPacketOutEntity.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntity.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntity.java
@@ -1,75 +1,75 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketOutEntity extends PacketEntityWrapper {
+	
+	Field yRot;
+	Field xRot;
+	Field onGround;
+	Field hasRot;
+	
+	WrappedPacketOutEntity(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntity",
+				networkProtocolGame + "PacketPlayOutEntity"
+			)
+		);
+		
+		yRot = getFirstField(
+			"yRot",
+			"yHeadRot",
+			"e",
+			"b"
+		);
+		xRot = getFirstField(
+			"xRot",
+			"f"
+		);
+		onGround = getFirstField(
+			"onGround",
+			"g"
+		);
+		hasRot = getFirstField(
+			"hasRot",
+			"h"
+		);
+	}
 
-    Field yRot;
-    Field xRot;
-    Field onGround;
-    Field hasRot;
+	public byte getYRot() {
+		return getFieldValue(yRot);
+	}
+	
+	public void setYRot(byte yRot) {
+		setField(this.yRot, yRot);
+	}
 
-    WrappedPacketOutEntity(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntity",
-                        networkProtocolGame + "PacketPlayOutEntity"
-                )
-        );
+	public byte getXRot() {
+		return getFieldValue(xRot);
+	}
+	
+	public void setXRot(byte xRot) {
+		setField(this.xRot, xRot);
+	}
 
-        yRot = getFirstField(
-                "yRot",
-                "yHeadRot",
-                "e",
-                "b"
-        );
-        xRot = getFirstField(
-                "xRot",
-                "f"
-        );
-        onGround = getFirstField(
-                "onGround",
-                "g"
-        );
-        hasRot = getFirstField(
-                "hasRot",
-                "h"
-        );
-    }
+	public boolean isOnGround() {
+		return getFieldValue(onGround);
+	}
+	
+	public void setOnGround(boolean onGround) {
+		setField(this.onGround, onGround);
+	}
 
-    public byte getYRot() {
-        return getFieldValue(yRot);
-    }
-
-    public void setYRot(byte yRot) {
-        setField(this.yRot, yRot);
-    }
-
-    public byte getXRot() {
-        return getFieldValue(xRot);
-    }
-
-    public void setXRot(byte xRot) {
-        setField(this.xRot, xRot);
-    }
-
-    public boolean isOnGround() {
-        return getFieldValue(onGround);
-    }
-
-    public void setOnGround(boolean onGround) {
-        setField(this.onGround, onGround);
-    }
-
-    public boolean isHasRot() {
-        return getFieldValue(hasRot);
-    }
-
-    public void setHasRot(boolean hasRot) {
-        setField(this.hasRot, hasRot);
-    }
+	public boolean isHasRot() {
+		return getFieldValue(hasRot);
+	}
+	
+	public void setHasRot(boolean hasRot) {
+		setField(this.hasRot, hasRot);
+	}
 }
