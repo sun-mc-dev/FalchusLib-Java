03aa3755d197b4f572b9821ce3bfd52aca61bda6
Elias <elias@falchus.com>
2026-04-10 23:04:15 +0200

    Update PacketUpdateTimeWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/time/PacketUpdateTimeWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/time/PacketUpdateTimeWrapper.java
@@ -1,44 +1,45 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.time;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketUpdateTimeWrapper extends PacketWrapper {
+	
+	Field gameTime;
+	Field dayTime;
 
-    Field gameTime;
-    Field dayTime;
+	PacketUpdateTimeWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		gameTime = getFirstField(
+			"gameTime",
+			"a"
+		);
+		dayTime = getFirstField(
+			"dayTime",
+			"b"
+		);
+	}
 
-    PacketUpdateTimeWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public long getGameTime() {
+		return getFieldValue(gameTime);
+	}
+	
+	public void setGameTime(long gameTime) {
+		setField(this.gameTime, gameTime);
+	}
 
-        gameTime = getFirstField(
-                "gameTime",
-                "a"
-        );
-        dayTime = getFirstField(
-                "dayTime",
-                "b"
-        );
-    }
-
-    public long getGameTime() {
-        return getFieldValue(gameTime);
-    }
-
-    public void setGameTime(long gameTime) {
-        setField(this.gameTime, gameTime);
-    }
-
-    public long getDayTime() {
-        return getFieldValue(dayTime);
-    }
-
-    public void setDayTime(long dayTime) {
-        setField(this.dayTime, dayTime);
-    }
+	public long getDayTime() {
+		return getFieldValue(dayTime);
+	}
+	
+	public void setDayTime(long dayTime) {
+		setField(this.dayTime, dayTime);
+	}
 }
