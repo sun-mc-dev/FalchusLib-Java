e138416c4b4bfb152bd3cb5037e931328902c1bb
Elias <elias@falchus.com>
2026-04-10 23:09:42 +0200

    Update Bossbar.java

--- src/com/falchus/lib/minecraft/spigot/player/elements/impl/Bossbar.java
+++ src/com/falchus/lib/minecraft/spigot/player/elements/impl/Bossbar.java
@@ -1,75 +1,77 @@
 package com.falchus.lib.minecraft.spigot.player.elements.impl;
 
+import java.util.function.Supplier;
+
+import org.bukkit.entity.Player;
+
 import com.falchus.lib.minecraft.spigot.enums.Version;
 import com.falchus.lib.minecraft.spigot.player.elements.PlayerElement;
 import com.falchus.lib.minecraft.spigot.utils.PlayerUtils;
 import com.falchus.lib.minecraft.spigot.utils.ServerUtils;
-import lombok.NonNull;
-import org.bukkit.entity.Player;
 
-import java.util.function.Supplier;
+import lombok.NonNull;
 
 public class Bossbar extends PlayerElement {
 
-    private Supplier<String> messageSupplier;
-    private Supplier<Double> progressSupplier;
-
-    private Bossbar(@NonNull Player player) {
-        super(player);
+	private Supplier<String> messageSupplier;
+	private Supplier<Double> progressSupplier;
+	
+	private Bossbar(@NonNull Player player) {
+    	super(player);
     }
-
-    /**
-     * Sends a one-time Bossbar message.
-     */
-    public void send(@NonNull Supplier<String> message, @NonNull Supplier<Double> progress) {
+	
+	/**
+	 * Sends a one-time Bossbar message.
+	 */
+	public void send(@NonNull Supplier<String> message, @NonNull Supplier<Double> progress) {
         messageSupplier = message;
         progressSupplier = progress;
-
+        
         updateRunnable = () -> {
-            String newMessage = messageSupplier.get();
-            double newProgress = progressSupplier.get();
-
-            PlayerUtils.sendBossbar(player, newMessage, newProgress);
+        	String newMessage = messageSupplier.get();
+        	double newProgress = progressSupplier.get();
+        	
+    		PlayerUtils.sendBossbar(player, newMessage, newProgress);
         };
         update();
-    }
-
-    /**
-     * Sends a Bossbar message repeatedly at a fixed interval.
-     */
-    public void sendUpdating(long intervalTicks, @NonNull Supplier<String> message, @NonNull Supplier<Double> progress) {
-        if (ServerUtils.getVersion().isBefore(Version.v1_17)) {
-            super.sendUpdating(intervalTicks, () ->
-                    send(
-                            message,
-                            progress
-                    )
-            );
-        } else {
-            send(
-                    message,
-                    progress
-            );
-        }
-    }
-
-    /**
-     * Removes the Bossbar, cancelling any ongoing update tasks.
-     */
-    @Override
-    public void remove() {
-        super.remove();
-
-        PlayerUtils.removeBossbar(player);
-    }
-
-    /**
-     * Sets the health/progress of the Bossbar.
-     */
-    public void setProgress(double progress) {
-        send(
-                messageSupplier,
-                () -> progress
-        );
-    }
+	}
+	
+	/**
+	 * Sends a Bossbar message repeatedly at a fixed interval.
+	 */
+	public void sendUpdating(long intervalTicks, @NonNull Supplier<String> message, @NonNull Supplier<Double> progress) {
+		if (ServerUtils.getVersion().isBefore(Version.v1_17)) {
+			super.sendUpdating(intervalTicks, () ->
+				send(
+					message,
+					progress
+				)
+			);
+		} else {
+			send(
+				message,
+				progress
+			);
+		}
+	}
+	
+	/**
+	 * Removes the Bossbar, cancelling any ongoing update tasks.
+	 */
+	@Override
+	public void remove() {
+		super.remove();
+		
+		PlayerUtils.removeBossbar(player);
+	}
+	
+	/**
+	 * Sets the health/progress of the Bossbar.
+	 */
+	public void setProgress(double progress) {
+		send(
+			messageSupplier,
+			() -> progress
+		);
+	}
 }
