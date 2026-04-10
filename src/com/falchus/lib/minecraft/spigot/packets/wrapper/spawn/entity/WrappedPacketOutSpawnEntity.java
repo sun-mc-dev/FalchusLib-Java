9b599d14a62eaa1dd6c7dc5813318309e2a80ab7
Elias <elias@falchus.com>
2026-04-10 22:57:50 +0200

    Update WrappedPacketOutSpawnEntity.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/entity/WrappedPacketOutSpawnEntity.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/entity/WrappedPacketOutSpawnEntity.java
@@ -1,17 +1,17 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.spawn.entity;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 // TODO: add all entity spawn packets
 public class WrappedPacketOutSpawnEntity extends PacketSpawnEntityWrapper {
 
-    public WrappedPacketOutSpawnEntity(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutSpawnEntity",
-                        networkProtocolGame + "PacketPlayOutSpawnEntity"
-                )
-        );
-    }
+	public WrappedPacketOutSpawnEntity(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutSpawnEntity",
+				networkProtocolGame + "PacketPlayOutSpawnEntity"
+			)
+		);
+	}
 }
