cbf45a7b3f3f0acc374e56c4ef6b1eea1d7d7b4b
Elias <elias@falchus.com>
2026-04-10 22:46:27 +0200

    Update PacketLoginWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/login/PacketLoginWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/login/PacketLoginWrapper.java
@@ -1,70 +1,71 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.login;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketLoginWrapper extends PacketWrapper {
+	
+	Field playerId;
+	Field hardcore;
+	Field maxPlayers;
+	Field reducedDebugInfo;
 
-    Field playerId;
-    Field hardcore;
-    Field maxPlayers;
-    Field reducedDebugInfo;
+	PacketLoginWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		playerId = getFirstField(
+			"playerId",
+			"a"
+		);
+		hardcore = getFirstField(
+			"hardcore",
+			"b"
+		);
+		maxPlayers = getFirstField(
+			"maxPlayers",
+			"f"
+		);
+		reducedDebugInfo = getFirstField(
+			"reducedDebugInfo",
+			"h"
+		);
+	}
 
-    PacketLoginWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getPlayerId() {
+		return getFieldValue(playerId);
+	}
+	
+	public void setPlayerId(int playerId) {
+		setField(this.playerId, playerId);
+	}
 
-        playerId = getFirstField(
-                "playerId",
-                "a"
-        );
-        hardcore = getFirstField(
-                "hardcore",
-                "b"
-        );
-        maxPlayers = getFirstField(
-                "maxPlayers",
-                "f"
-        );
-        reducedDebugInfo = getFirstField(
-                "reducedDebugInfo",
-                "h"
-        );
-    }
+	public boolean getHardcore() {
+		return getFieldValue(hardcore);
+	}
+	
+	public void setHardcore(boolean hardcore) {
+		setField(this.hardcore, hardcore);
+	}
 
-    public int getPlayerId() {
-        return getFieldValue(playerId);
-    }
+	public int getMaxPlayers() {
+		return getFieldValue(maxPlayers);
+	}
+	
+	public void setMaxPlayers(int maxPlayers) {
+		setField(this.maxPlayers, maxPlayers);
+	}
 
-    public void setPlayerId(int playerId) {
-        setField(this.playerId, playerId);
-    }
-
-    public boolean getHardcore() {
-        return getFieldValue(hardcore);
-    }
-
-    public void setHardcore(boolean hardcore) {
-        setField(this.hardcore, hardcore);
-    }
-
-    public int getMaxPlayers() {
-        return getFieldValue(maxPlayers);
-    }
-
-    public void setMaxPlayers(int maxPlayers) {
-        setField(this.maxPlayers, maxPlayers);
-    }
-
-    public boolean isReducedDebugInfo() {
-        return getFieldValue(reducedDebugInfo);
-    }
-
-    public void setReducedDebugInfo(boolean reducedDebugInfo) {
-        setField(this.reducedDebugInfo, reducedDebugInfo);
-    }
+	public boolean isReducedDebugInfo() {
+		return getFieldValue(reducedDebugInfo);
+	}
+	
+	public void setReducedDebugInfo(boolean reducedDebugInfo) {
+		setField(this.reducedDebugInfo, reducedDebugInfo);
+	}
 }
