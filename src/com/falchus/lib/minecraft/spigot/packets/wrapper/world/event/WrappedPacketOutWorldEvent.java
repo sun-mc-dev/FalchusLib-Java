c71db3ae09c4d0e9986ad38b80e0fa864277524a
Elias <elias@falchus.com>
2026-04-10 23:08:35 +0200

    Update WrappedPacketOutWorldEvent.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/world/event/WrappedPacketOutWorldEvent.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/world/event/WrappedPacketOutWorldEvent.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.world.event;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutWorldEvent extends PacketWorldEventWrapper {
 
-    public WrappedPacketOutWorldEvent(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutWorldEvent",
-                        networkProtocolGame + "PacketPlayOutWorldEvent"
-                )
-        );
-    }
+	public WrappedPacketOutWorldEvent(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutWorldEvent",
+				networkProtocolGame + "PacketPlayOutWorldEvent"
+			)
+		);
+	}
 }
