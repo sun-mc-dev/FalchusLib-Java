3620a7b0e9864039752cd1950804979f150651d3
Elias <elias@falchus.com>
2026-04-10 23:03:13 +0200

    Update PacketUpdateHealthWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/health/PacketUpdateHealthWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/health/PacketUpdateHealthWrapper.java
@@ -1,57 +1,58 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.health;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketUpdateHealthWrapper extends PacketWrapper {
+	
+	Field health;
+	Field food;
+	Field saturation;
+	
+	PacketUpdateHealthWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		health = getFirstField(
+			"health",
+			"a"
+		);
+		food = getFirstField(
+			"food",
+			"b"
+		);
+		saturation = getFirstField(
+			"saturation",
+			"c"
+		);
+	}
 
-    Field health;
-    Field food;
-    Field saturation;
+	public float getHealth() {
+		return getFieldValue(health);
+	}
+	
+	public void setHealth(float health) {
+		setField(this.health, health);
+	}
 
-    PacketUpdateHealthWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getFood() {
+		return getFieldValue(food);
+	}
+	
+	public void setFood(int food) {
+		setField(this.food, food);
+	}
 
-        health = getFirstField(
-                "health",
-                "a"
-        );
-        food = getFirstField(
-                "food",
-                "b"
-        );
-        saturation = getFirstField(
-                "saturation",
-                "c"
-        );
-    }
-
-    public float getHealth() {
-        return getFieldValue(health);
-    }
-
-    public void setHealth(float health) {
-        setField(this.health, health);
-    }
-
-    public int getFood() {
-        return getFieldValue(food);
-    }
-
-    public void setFood(int food) {
-        setField(this.food, food);
-    }
-
-    public float getSaturation() {
-        return getFieldValue(saturation);
-    }
-
-    public void setSaturation(int saturation) {
-        setField(this.saturation, saturation);
-    }
+	public float getSaturation() {
+		return getFieldValue(saturation);
+	}
+	
+	public void setSaturation(int saturation) {
+		setField(this.saturation, saturation);
+	}
 }
