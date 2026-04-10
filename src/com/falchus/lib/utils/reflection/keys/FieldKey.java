56639c2a25e2b626f64f13122540d9872a503f85
Elias <elias@falchus.com>
2026-04-10 23:23:45 +0200

    Update FieldKey.java

--- src/com/falchus/lib/utils/reflection/keys/FieldKey.java
+++ src/com/falchus/lib/utils/reflection/keys/FieldKey.java
@@ -1,20 +1,20 @@
 package com.falchus.lib.utils.reflection.keys;
 
-import lombok.NonNull;
-
 import java.util.Set;
 
+import lombok.NonNull;
+
 public record FieldKey(@NonNull Set<Class<?>> classes, @NonNull Set<String> names) {
-
-    public FieldKey(@NonNull Class<?> clazz, @NonNull String name) {
-        this(Set.of(clazz), Set.of(name));
-    }
-
-    public FieldKey(@NonNull Class<?> clazz, @NonNull String... names) {
-        this(Set.of(clazz), Set.of(names));
-    }
-
-    public FieldKey(@NonNull Set<Class<?>> classes, @NonNull String... names) {
-        this(classes, Set.of(names));
-    }
+	
+	public FieldKey(@NonNull Class<?> clazz, @NonNull String name) {
+		this(Set.of(clazz), Set.of(name));
+	}
+	
+	public FieldKey(@NonNull Class<?> clazz, @NonNull String... names) {
+		this(Set.of(clazz), Set.of(names));
+	}
+	
+	public FieldKey(@NonNull Set<Class<?>> classes, @NonNull String... names) {
+		this(classes, Set.of(names));
+	}
 }
