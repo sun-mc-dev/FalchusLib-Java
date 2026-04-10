f698e73cd0f2704518dd7e01cbe6252ba2b3c1fd
Elias <elias@falchus.com>
2026-04-10 23:02:09 +0200

    Update WrappedPacketOutSubtitleTitle.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/title/WrappedPacketOutSubtitleTitle.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/title/WrappedPacketOutSubtitleTitle.java
@@ -1,40 +1,40 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.title;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketOutSubtitleTitle extends PacketTitleWrapper {
+	
+	Field text;
+	
+	WrappedPacketOutSubtitleTitle(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutTitle",
+				networkProtocolGame + "ClientboundSetSubtitleTextPacket"
+			)
+		);
+		
+		text = getFirstField(
+			"text",
+			"b"
+		);
+	}
 
-    Field text;
-
-    WrappedPacketOutSubtitleTitle(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutTitle",
-                        networkProtocolGame + "ClientboundSetSubtitleTextPacket"
-                )
-        );
-
-        text = getFirstField(
-                "text",
-                "b"
-        );
-    }
-
-    /**
-     * @return IChatBaseComponent
-     */
-    public Object getText() {
-        return getFieldValue(text);
-    }
-
-    /**
-     * @param text: IChatBaseComponent
-     */
-    public void setText(Object text) {
-        setField(this.text, text);
-    }
+	/**
+	 * @return IChatBaseComponent
+	 */
+	public Object getText() {
+		return getFieldValue(text);
+	}
+	
+	/**
+	 * @param text: IChatBaseComponent
+	 */
+	public void setText(Object text) {
+		setField(this.text, text);
+	}
 }
