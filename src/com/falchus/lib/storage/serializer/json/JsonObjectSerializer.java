e1e6f5f13c3c3498a772e632512df955c3a56762
Elias <elias@falchus.com>
2026-04-10 23:21:22 +0200

    Update JsonObjectSerializer.java

--- src/com/falchus/lib/storage/serializer/json/JsonObjectSerializer.java
+++ src/com/falchus/lib/storage/serializer/json/JsonObjectSerializer.java
@@ -3,26 +3,26 @@ package com.falchus.lib.storage.serializer.json;
 import org.json.simple.JSONObject;
 
 public abstract class JsonObjectSerializer<T> extends JsonSerializer<T, JSONObject> {
-
-    @Override
-    public String serialize(T value) {
-        return to(value).toJSONString();
-    }
-
-    @Override
-    public T deserialize(String content) {
-        if (content == null || content.isBlank()) {
-            return from(new JSONObject());
-        }
-
-        try {
-            Object raw = parser.parse(content);
-            if (raw instanceof JSONObject json) {
-                return from(json);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return from(new JSONObject());
-    }
+	
+	@Override
+	public String serialize(T value) {
+		return to(value).toJSONString();
+	}
+	
+	@Override
+	public T deserialize(String content) {
+		if (content == null || content.isBlank()) {
+			return from(new JSONObject());
+		}
+		
+		try {
+			Object raw = parser.parse(content);
+			if (raw instanceof JSONObject json) {
+				return from(json);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return from(new JSONObject());
+	}
 }
