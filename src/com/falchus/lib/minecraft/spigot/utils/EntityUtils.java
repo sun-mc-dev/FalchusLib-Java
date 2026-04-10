7df1826441876e6345c339266ca841c8dec22db1
Elias <elias@falchus.com>
2026-04-10 23:17:33 +0200

    Update EntityUtils.java

--- src/com/falchus/lib/minecraft/spigot/utils/EntityUtils.java
+++ src/com/falchus/lib/minecraft/spigot/utils/EntityUtils.java
@@ -1,24 +1,26 @@
 package com.falchus.lib.minecraft.spigot.utils;
 
+import org.bukkit.entity.Entity;
+
 import com.falchus.lib.minecraft.spigot.utils.version.VersionProvider;
+
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
-import org.bukkit.entity.Entity;
 
 @UtilityClass
 public class EntityUtils {
 
-    /**
-     * @return {@link Entity}
-     */
-    public static Entity getBukkitEntity(@NonNull Object entity) {
-        return VersionProvider.get().getBukkitEntity(entity);
-    }
-
-    /**
-     * Sets yaw and pitch.
-     */
-    public static void setYawPitch(@NonNull Object entity, float yaw, float pitch) {
-        VersionProvider.get().setYawPitch(entity, yaw, pitch);
-    }
+	/**
+	 * @return {@link Entity}
+	 */
+	public static Entity getBukkitEntity(@NonNull Object entity) {
+		return VersionProvider.get().getBukkitEntity(entity);
+	}
+	
+	/**
+	 * Sets yaw and pitch.
+	 */
+	public static void setYawPitch(@NonNull Object entity, float yaw, float pitch) {
+		VersionProvider.get().setYawPitch(entity, yaw, pitch);
+	}
 }
