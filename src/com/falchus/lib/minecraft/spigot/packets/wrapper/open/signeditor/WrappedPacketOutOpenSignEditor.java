f2fb1717027af402f90646b5322b9ae526313d05
Elias <elias@falchus.com>
2026-04-10 22:48:55 +0200

    Update WrappedPacketOutOpenSignEditor.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/open/signeditor/WrappedPacketOutOpenSignEditor.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/open/signeditor/WrappedPacketOutOpenSignEditor.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.open.signeditor;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutOpenSignEditor extends PacketOpenSignEditorWrapper {
 
-    public WrappedPacketOutOpenSignEditor(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutOpenSignEditor",
-                        networkProtocolGame + "PacketPlayOutOpenSignEditor"
-                )
-        );
-    }
+	public WrappedPacketOutOpenSignEditor(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutOpenSignEditor",
+				networkProtocolGame + "PacketPlayOutOpenSignEditor"
+			)
+		);
+	}
 }
