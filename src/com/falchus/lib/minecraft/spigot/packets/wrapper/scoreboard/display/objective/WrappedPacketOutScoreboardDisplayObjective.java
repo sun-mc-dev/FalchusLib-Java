71279d51b0ffb282f67aa57fe882ebc651264c86
Elias <elias@falchus.com>
2026-04-10 22:53:12 +0200

    Update WrappedPacketOutScoreboardDisplayObjective.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/display/objective/WrappedPacketOutScoreboardDisplayObjective.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/display/objective/WrappedPacketOutScoreboardDisplayObjective.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.display.objective;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutScoreboardDisplayObjective extends PacketScoreboardDisplayObjectiveWrapper {
 
-    WrappedPacketOutScoreboardDisplayObjective(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutScoreboardDisplayObjective",
-                        networkProtocolGame + "PacketPlayOutScoreboardDisplayObjective"
-                )
-        );
-    }
+	WrappedPacketOutScoreboardDisplayObjective(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutScoreboardDisplayObjective",
+				networkProtocolGame + "PacketPlayOutScoreboardDisplayObjective"
+			)
+		);
+	}
 }
