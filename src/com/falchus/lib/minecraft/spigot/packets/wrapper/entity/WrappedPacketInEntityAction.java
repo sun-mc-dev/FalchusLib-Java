42806b59cef49efb2dac6095de1d044b366ceb5f
Elias <elias@falchus.com>
2026-04-10 22:38:17 +0200

    Update WrappedPacketInEntityAction.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketInEntityAction.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketInEntityAction.java
@@ -1,53 +1,53 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketInEntityAction extends PacketEntityWrapper {
 
-    Field action;
-    Field data;
+	Field action;
+	Field data;
+	
+	WrappedPacketInEntityAction(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInEntityAction",
+				networkProtocolGame + "PacketPlayInEntityAction"
+			)
+		);
+		
+		action = getFirstField(
+			"action",
+			"animation"
+		);
+		data = getFirstField(
+			"data",
+			"c"
+		);
+	}
 
-    WrappedPacketInEntityAction(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInEntityAction",
-                        networkProtocolGame + "PacketPlayInEntityAction"
-                )
-        );
+	/**
+	 * @return PacketPlayInEntityAction$EnumPlayerAction
+	 */
+	public Object getAction() {
+		return getFieldValue(action);
+	}
+	
+	/**
+	 * @param action: PacketPlayInEntityAction$EnumPlayerAction
+	 */
+	public void setAction(Object action) {
+		setField(this.action, action);
+	}
 
-        action = getFirstField(
-                "action",
-                "animation"
-        );
-        data = getFirstField(
-                "data",
-                "c"
-        );
-    }
-
-    /**
-     * @return PacketPlayInEntityAction$EnumPlayerAction
-     */
-    public Object getAction() {
-        return getFieldValue(action);
-    }
-
-    /**
-     * @param action: PacketPlayInEntityAction$EnumPlayerAction
-     */
-    public void setAction(Object action) {
-        setField(this.action, action);
-    }
-
-    public int getData() {
-        return getFieldValue(data);
-    }
-
-    public void setData(int data) {
-        setField(this.data, data);
-    }
+	public int getData() {
+		return getFieldValue(data);
+	}
+	
+	public void setData(int data) {
+		setField(this.data, data);
+	}
 }
