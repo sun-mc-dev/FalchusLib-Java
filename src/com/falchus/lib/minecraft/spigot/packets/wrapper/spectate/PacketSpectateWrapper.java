0920ec2579a16557585a6bb7bdefd34188566436
Elias <elias@falchus.com>
2026-04-10 22:58:26 +0200

    Update PacketSpectateWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/spectate/PacketSpectateWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/spectate/PacketSpectateWrapper.java
@@ -1,32 +1,33 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.spectate;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.UUID;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketSpectateWrapper extends PacketWrapper {
+	
+	Field uuid;
 
-    Field uuid;
+	PacketSpectateWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		uuid = getFirstField(
+			"uuid",
+			"a"
+		);
+	}
 
-    PacketSpectateWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        uuid = getFirstField(
-                "uuid",
-                "a"
-        );
-    }
-
-    public UUID getUUID() {
-        return getFieldValue(uuid);
-    }
-
-    public void setUUID(int cameraId) {
-        setField(this.uuid, cameraId);
-    }
+	public UUID getUUID() {
+		return getFieldValue(uuid);
+	}
+	
+	public void setUUID(int cameraId) {
+		setField(this.uuid, cameraId);
+	}
 }
