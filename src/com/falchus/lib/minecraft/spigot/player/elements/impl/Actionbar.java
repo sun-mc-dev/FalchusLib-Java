9273b48df063a1b0af19f141ffb25d9af3c43281
Elias <elias@falchus.com>
2026-04-10 23:09:30 +0200

    Update Actionbar.java

--- src/com/falchus/lib/minecraft/spigot/player/elements/impl/Actionbar.java
+++ src/com/falchus/lib/minecraft/spigot/player/elements/impl/Actionbar.java
@@ -1,42 +1,44 @@
 package com.falchus.lib.minecraft.spigot.player.elements.impl;
 
-import com.falchus.lib.minecraft.spigot.player.elements.PlayerElement;
-import com.falchus.lib.minecraft.spigot.utils.PlayerUtils;
-import lombok.NonNull;
-import org.bukkit.entity.Player;
-
 import java.util.function.Supplier;
 
+import org.bukkit.entity.Player;
+
+import com.falchus.lib.minecraft.spigot.player.elements.PlayerElement;
+import com.falchus.lib.minecraft.spigot.utils.PlayerUtils;
+
+import lombok.NonNull;
+
 public class Actionbar extends PlayerElement {
+	
+	private Supplier<String> messageSupplier;
 
-    private Supplier<String> messageSupplier;
-
-    private Actionbar(@NonNull Player player) {
-        super(player);
-    }
-
-    /**
-     * Sends a one-time action bar message.
-     */
-    public void send(@NonNull Supplier<String> message) {
-        messageSupplier = message;
-
-        updateRunnable = () -> {
-            String newMessage = messageSupplier.get();
-
-            PlayerUtils.sendActionbar(player, newMessage);
-        };
-        update();
-    }
-
-    /**
-     * Sends an action bar message repeatedly at a fixed interval.
-     */
-    public void sendUpdating(long intervalTicks, @NonNull Supplier<String> message) {
-        super.sendUpdating(intervalTicks, () ->
-                send(
-                        message
-                )
-        );
-    }
+	private Actionbar(@NonNull Player player) {
+		super(player);
+	}
+	
+	/**
+	 * Sends a one-time action bar message.
+	 */
+	public void send(@NonNull Supplier<String> message) {
+		messageSupplier = message;
+		
+		updateRunnable = () -> {
+			String newMessage = messageSupplier.get();
+			
+			PlayerUtils.sendActionbar(player, newMessage);
+		};
+		update();
+	}
+	
+	/**
+	 * Sends an action bar message repeatedly at a fixed interval.
+	 */
+	public void sendUpdating(long intervalTicks, @NonNull Supplier<String> message) {
+		super.sendUpdating(intervalTicks, () ->
+			send(
+				message
+			)
+		);
+	}
 }
