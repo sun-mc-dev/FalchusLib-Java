63fbc638037e86e4d7dfbb84a28cb3c6f802fa93
Elias <elias@falchus.com>
2026-04-10 22:33:34 +0200

    Update PacketCameraWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/camera/PacketCameraWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/camera/PacketCameraWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.camera;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketCameraWrapper extends PacketWrapper {
+	
+	Field cameraId;
 
-    Field cameraId;
+	PacketCameraWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		cameraId = getFirstField(
+			"cameraId",
+			"a"
+		);
+	}
 
-    PacketCameraWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        cameraId = getFirstField(
-                "cameraId",
-                "a"
-        );
-    }
-
-    public int getCameraId() {
-        return getFieldValue(cameraId);
-    }
-
-    public void setCameraId(int cameraId) {
-        setField(this.cameraId, cameraId);
-    }
+	public int getCameraId() {
+		return getFieldValue(cameraId);
+	}
+	
+	public void setCameraId(int cameraId) {
+		setField(this.cameraId, cameraId);
+	}
 }
