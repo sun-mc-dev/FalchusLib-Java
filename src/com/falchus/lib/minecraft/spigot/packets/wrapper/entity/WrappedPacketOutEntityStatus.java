f5f416ede1f8b397ecf402eb2f2402a65391aa7e
Elias <elias@falchus.com>
2026-04-10 22:40:36 +0200

    Update WrappedPacketOutEntityStatus.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityStatus.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityStatus.java
@@ -1,34 +1,34 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketOutEntityStatus extends PacketEntityWrapper {
 
-    Field eventId;
+	Field eventId;
+	
+	WrappedPacketOutEntityStatus(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntityStatus",
+				networkProtocolGame + "PacketPlayOutEntityStatus"
+			)
+		);
+		
+		eventId = getFirstField(
+			"eventId",
+			"b"
+		);
+	}
 
-    WrappedPacketOutEntityStatus(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntityStatus",
-                        networkProtocolGame + "PacketPlayOutEntityStatus"
-                )
-        );
-
-        eventId = getFirstField(
-                "eventId",
-                "b"
-        );
-    }
-
-    public byte getEventId() {
-        return getFieldValue(eventId);
-    }
-
-    public void setEventId(byte eventId) {
-        setField(this.eventId, eventId);
-    }
+	public byte getEventId() {
+		return getFieldValue(eventId);
+	}
+	
+	public void setEventId(byte eventId) {
+		setField(this.eventId, eventId);
+	}
 }
