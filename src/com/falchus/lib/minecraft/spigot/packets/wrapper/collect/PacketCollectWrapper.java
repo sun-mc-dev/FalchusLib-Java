fc115e2dc7ddaf1e48bed42a2bdac987d04eb6a2
Elias <elias@falchus.com>
2026-04-10 22:36:16 +0200

    Update PacketCollectWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/collect/PacketCollectWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/collect/PacketCollectWrapper.java
@@ -1,44 +1,45 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.collect;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketCollectWrapper extends PacketWrapper {
+	
+	Field itemId;
+	Field playerId;
 
-    Field itemId;
-    Field playerId;
+	PacketCollectWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		itemId = getFirstField(
+			"itemId",
+			"a"
+		);
+		playerId = getFirstField(
+			"playerId",
+			"b"
+		);
+	}
 
-    PacketCollectWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getItemId() {
+		return getFieldValue(itemId);
+	}
+	
+	public void setItemId(int itemId) {
+		setField(this.itemId, itemId);
+	}
 
-        itemId = getFirstField(
-                "itemId",
-                "a"
-        );
-        playerId = getFirstField(
-                "playerId",
-                "b"
-        );
-    }
-
-    public int getItemId() {
-        return getFieldValue(itemId);
-    }
-
-    public void setItemId(int itemId) {
-        setField(this.itemId, itemId);
-    }
-
-    public int getPlayerId() {
-        return getFieldValue(playerId);
-    }
-
-    public void setPlayerId(int playerId) {
-        setField(this.playerId, playerId);
-    }
+	public int getPlayerId() {
+		return getFieldValue(playerId);
+	}
+	
+	public void setPlayerId(int playerId) {
+		setField(this.playerId, playerId);
+	}
 }
