95b3866973176c449fd5383c590d7ea0ee72ff35
Elias <elias@falchus.com>
2026-04-10 22:57:36 +0200

    Update PacketSpawnEntityWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/entity/PacketSpawnEntityWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/entity/PacketSpawnEntityWrapper.java
@@ -1,44 +1,45 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.spawn.entity;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 abstract class PacketSpawnEntityWrapper extends PacketWrapper {
 
-    Field id;
-    Field data;
+	Field id;
+	Field data;
 
-    PacketSpawnEntityWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	PacketSpawnEntityWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
 
-        id = getFirstField(
-                "id",
-                "a"
-        );
-        data = getFirstField(
-                "data",
-                "k"
-        );
-    }
+		id = getFirstField(
+			"id",
+			"a"
+		);
+		data = getFirstField(
+			"data",
+			"k"
+		);
+	}
 
-    public int getId() {
-        return getFieldValue(id);
-    }
+	public int getId() {
+		return getFieldValue(id);
+	}
 
-    public void setId(int id) {
-        setField(this.id, id);
-    }
+	public void setId(int id) {
+		setField(this.id, id);
+	}
 
-    public int getData() {
-        return getFieldValue(data);
-    }
+	public int getData() {
+		return getFieldValue(data);
+	}
 
-    public void setData(int data) {
-        setField(this.data, data);
-    }
+	public void setData(int data) {
+		setField(this.data, data);
+	}
 }
