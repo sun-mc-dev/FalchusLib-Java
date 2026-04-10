8754f441246b03dee49e973f693ee8718a4c506d
Elias <elias@falchus.com>
2026-04-10 22:37:29 +0200

    Update PacketEnchantItemWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/enchantitem/PacketEnchantItemWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/enchantitem/PacketEnchantItemWrapper.java
@@ -1,44 +1,45 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.enchantitem;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketEnchantItemWrapper extends PacketWrapper {
+	
+	Field containerId;
+	Field buttonId;
 
-    Field containerId;
-    Field buttonId;
+	PacketEnchantItemWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		containerId = getFirstField(
+			"containerId",
+			"a"
+		);
+		buttonId = getFirstField(
+			"buttonId",
+			"b"
+		);
+	}
 
-    PacketEnchantItemWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getContainerId() {
+		return getFieldValue(containerId);
+	}
+	
+	public void setContainerId(int containerId) {
+		setField(this.containerId, containerId);
+	}
 
-        containerId = getFirstField(
-                "containerId",
-                "a"
-        );
-        buttonId = getFirstField(
-                "buttonId",
-                "b"
-        );
-    }
-
-    public int getContainerId() {
-        return getFieldValue(containerId);
-    }
-
-    public void setContainerId(int containerId) {
-        setField(this.containerId, containerId);
-    }
-
-    public int getButtonId() {
-        return getFieldValue(buttonId);
-    }
-
-    public void setButtonId(int buttonId) {
-        setField(this.buttonId, buttonId);
-    }
+	public int getButtonId() {
+		return getFieldValue(buttonId);
+	}
+	
+	public void setButtonId(int buttonId) {
+		setField(this.buttonId, buttonId);
+	}
 }
