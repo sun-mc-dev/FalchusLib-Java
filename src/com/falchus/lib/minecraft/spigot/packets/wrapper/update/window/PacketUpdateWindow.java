f1dfb5cc372a985eedb16035a3805b3fce24b48c
Elias <elias@falchus.com>
2026-04-10 23:04:40 +0200

    Update PacketUpdateWindow.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/window/PacketUpdateWindow.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/window/PacketUpdateWindow.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.window;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketUpdateWindow extends PacketWrapper {
+	
+	Field containerId;
 
-    Field containerId;
+	PacketUpdateWindow(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		containerId = getFirstField(
+			"containerId",
+			"a"
+		);
+	}
 
-    PacketUpdateWindow(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        containerId = getFirstField(
-                "containerId",
-                "a"
-        );
-    }
-
-    public int getContainerId() {
-        return getFieldValue(containerId);
-    }
-
-    public void setContainerId(int containerId) {
-        setField(this.containerId, containerId);
-    }
+	public int getContainerId() {
+		return getFieldValue(containerId);
+	}
+	
+	public void setContainerId(int containerId) {
+		setField(this.containerId, containerId);
+	}
 }
