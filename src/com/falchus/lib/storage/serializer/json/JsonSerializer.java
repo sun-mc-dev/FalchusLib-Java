f18709d4a84eef7f4e74748448099388da403d8b
Elias <elias@falchus.com>
2026-04-10 23:21:37 +0200

    Update JsonSerializer.java

--- src/com/falchus/lib/storage/serializer/json/JsonSerializer.java
+++ src/com/falchus/lib/storage/serializer/json/JsonSerializer.java
@@ -1,10 +1,11 @@
 package com.falchus.lib.storage.serializer.json;
 
-import com.falchus.lib.interfaces.Mapper;
-import com.falchus.lib.storage.serializer.Serializer;
 import org.json.simple.parser.JSONParser;
 
-abstract class JsonSerializer<T, U> implements Mapper<T, U>, Serializer<T> {
+import com.falchus.lib.interfaces.Mapper;
+import com.falchus.lib.storage.serializer.Serializer;
 
-    final JSONParser parser = new JSONParser();
+abstract class JsonSerializer<T, U> implements Mapper<T, U>, Serializer<T> {
+	
+	final JSONParser parser = new JSONParser();
 }
