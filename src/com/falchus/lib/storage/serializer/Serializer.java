6d4ebac2154afb8f530be1ee8cb1f795d755c276
Elias <elias@falchus.com>
2026-04-10 23:21:48 +0200

    Update Serializer.java

--- src/com/falchus/lib/storage/serializer/Serializer.java
+++ src/com/falchus/lib/storage/serializer/Serializer.java
@@ -2,7 +2,7 @@ package com.falchus.lib.storage.serializer;
 
 public interface Serializer<T> {
 
-    String serialize(T value);
-
-    T deserialize(String content);
+	String serialize(T value);
+	
+	T deserialize(String content);
 }
