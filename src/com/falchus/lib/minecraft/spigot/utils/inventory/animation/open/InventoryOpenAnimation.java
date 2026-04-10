ea5cd1c20c3365abf00d80197cb26d015a5d9660
Elias <elias@falchus.com>
2026-04-10 23:12:45 +0200

    Update InventoryOpenAnimation.java

--- src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/InventoryOpenAnimation.java
+++ src/com/falchus/lib/minecraft/spigot/utils/inventory/animation/open/InventoryOpenAnimation.java
@@ -3,5 +3,5 @@ package com.falchus.lib.minecraft.spigot.utils.inventory.animation.open;
 import com.falchus.lib.minecraft.spigot.utils.inventory.animation.InventoryAnimation;
 
 public abstract class InventoryOpenAnimation extends InventoryAnimation {
-
+	
 }
