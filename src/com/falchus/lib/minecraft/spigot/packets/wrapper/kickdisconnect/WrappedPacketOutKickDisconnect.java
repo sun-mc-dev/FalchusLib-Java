5ad15a59c76f0cd2b543afb0ad015e3d29c1cc11
Elias <elias@falchus.com>
2026-04-10 22:45:58 +0200

    Update WrappedPacketOutKickDisconnect.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/kickdisconnect/WrappedPacketOutKickDisconnect.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/kickdisconnect/WrappedPacketOutKickDisconnect.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.kickdisconnect;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutKickDisconnect extends PacketKickDisconnectWrapper {
 
-    WrappedPacketOutKickDisconnect(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutKickDisconnect",
-                        networkProtocolCommon + "ClientboundDisconnectPacket"
-                )
-        );
-    }
+	WrappedPacketOutKickDisconnect(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutKickDisconnect",
+				networkProtocolCommon + "ClientboundDisconnectPacket"
+			)
+		);
+	}
 }
