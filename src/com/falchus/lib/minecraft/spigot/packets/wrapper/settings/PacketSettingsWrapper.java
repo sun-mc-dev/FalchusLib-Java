53ad04e2d892a43d9eacc33da514dded928144ce
Elias <elias@falchus.com>
2026-04-10 22:56:59 +0200

    Update PacketSettingsWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/settings/PacketSettingsWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/settings/PacketSettingsWrapper.java
@@ -1,13 +1,14 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.settings;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-
 import java.util.Set;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
 
 abstract class PacketSettingsWrapper extends PacketWrapper {
 
-    PacketSettingsWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-    }
+	PacketSettingsWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+	}
 }
