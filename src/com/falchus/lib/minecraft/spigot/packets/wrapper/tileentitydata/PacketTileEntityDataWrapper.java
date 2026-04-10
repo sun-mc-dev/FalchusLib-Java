657d4cd261e372d7a1e4df2ebd8ec73e5a342670
Elias <elias@falchus.com>
2026-04-10 23:01:07 +0200

    Update PacketTileEntityDataWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/tileentitydata/PacketTileEntityDataWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/tileentitydata/PacketTileEntityDataWrapper.java
@@ -1,69 +1,70 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.tileentitydata;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketTileEntityDataWrapper extends PacketWrapper {
+	
+	Field pos;
+	Field type;
+	Field tag;
 
-    Field pos;
-    Field type;
-    Field tag;
+	PacketTileEntityDataWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		pos = getFirstField(
+			"pos",
+			"a"
+		);
+		type = getFirstField(
+			"type",
+			"b"
+		);
+		tag = getFirstField(
+			"tag",
+			"c"
+		);
+	}
 
-    PacketTileEntityDataWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	/**
+	 * @return BlockPosition
+	 */
+	public Object getPos() {
+		return getFieldValue(pos);
+	}
+	
+	/**
+	 * @param pos: BlockPosition
+	 */
+	public void setPos(Object pos) {
+		setField(this.pos, pos);
+	}
 
-        pos = getFirstField(
-                "pos",
-                "a"
-        );
-        type = getFirstField(
-                "type",
-                "b"
-        );
-        tag = getFirstField(
-                "tag",
-                "c"
-        );
-    }
+	public int getType() {
+		return getFieldValue(type);
+	}
+	
+	public void setType(int type) {
+		setField(this.type, type);
+	}
 
-    /**
-     * @return BlockPosition
-     */
-    public Object getPos() {
-        return getFieldValue(pos);
-    }
-
-    /**
-     * @param pos: BlockPosition
-     */
-    public void setPos(Object pos) {
-        setField(this.pos, pos);
-    }
-
-    public int getType() {
-        return getFieldValue(type);
-    }
-
-    public void setType(int type) {
-        setField(this.type, type);
-    }
-
-    /**
-     * @return NBTTagCompound
-     */
-    public Object getTag() {
-        return getFieldValue(tag);
-    }
-
-    /**
-     * @param tag: NBTTagCompound
-     */
-    public void setTag(Object tag) {
-        setField(this.tag, tag);
-    }
+	/**
+	 * @return NBTTagCompound
+	 */
+	public Object getTag() {
+		return getFieldValue(tag);
+	}
+	
+	/**
+	 * @param tag: NBTTagCompound
+	 */
+	public void setTag(Object tag) {
+		setField(this.tag, tag);
+	}
 }
