fcfc097faabb976f9561292da9c5f3bc8b5ab1d7
Elias <elias@falchus.com>
2026-04-10 22:36:28 +0200

    Update WrappedPacketOutCollect.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/collect/WrappedPacketOutCollect.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/collect/WrappedPacketOutCollect.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.collect;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutCollect extends PacketCollectWrapper {
 
-    public WrappedPacketOutCollect(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutCollect",
-                        networkProtocolGame + "PacketPlayOutCollect"
-                )
-        );
-    }
+	public WrappedPacketOutCollect(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutCollect",
+				networkProtocolGame + "PacketPlayOutCollect"
+			)
+		);
+	}
 }
