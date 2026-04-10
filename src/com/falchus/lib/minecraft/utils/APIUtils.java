6b464174e11cedb8a05f0882779d6ad35536404e
Elias <elias@falchus.com>
2026-04-10 23:19:43 +0200

    Update APIUtils.java

--- src/com/falchus/lib/minecraft/utils/APIUtils.java
+++ src/com/falchus/lib/minecraft/utils/APIUtils.java
@@ -1,45 +1,46 @@
 package com.falchus.lib.minecraft.utils;
 
+import java.util.UUID;
+
 import com.falchus.lib.utils.http.HTTPRequest;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
-
-import java.util.UUID;
 
 @UtilityClass
 public class APIUtils {
 
-    /**
-     * Gets the UUID of a player.
-     *
-     * @return the player's {@link UUID}
-     */
+	/**
+	 * Gets the UUID of a player.
+	 * 
+	 * @return the player's {@link UUID}
+	 */
     public static UUID getUUID(@NonNull String username) {
-        String body = HTTPRequest.get("https://api.mojang.com/users/profiles/minecraft/" + username);
+    	String body = HTTPRequest.get("https://api.mojang.com/users/profiles/minecraft/" + username);
         if (body == null) return null;
 
-        JsonObject json = JsonParser.parseString(body).getAsJsonObject();
-        String id = json.get("id").getAsString();
-
+    	JsonObject json = JsonParser.parseString(body).getAsJsonObject();
+    	String id = json.get("id").getAsString();
+    	
         String uuid = id.replaceFirst(
-                "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w+)",
-                "$1-$2-$3-$4-$5"
+            "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w+)",
+            "$1-$2-$3-$4-$5"
         );
         return UUID.fromString(uuid);
     }
-
+    
     /**
      * Gets the name of a player by UUID.
-     *
+     * 
      * @return the player's username {@link String}
-     */
+	 */
     public static String getName(@NonNull String uuid) {
         String body = HTTPRequest.get("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.replace("-", ""));
         if (body == null) return null;
 
-        JsonObject json = JsonParser.parseString(body).getAsJsonObject();
-        return json.get("name").getAsString();
+    	JsonObject json = JsonParser.parseString(body).getAsJsonObject();
+    	return json.get("name").getAsString();
     }
 }
