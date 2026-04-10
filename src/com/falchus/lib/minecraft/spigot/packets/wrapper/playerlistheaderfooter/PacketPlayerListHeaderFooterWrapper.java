1173d6be471fa33e9939b05f9f0a546800b0370e
Elias <elias@falchus.com>
2026-04-10 22:49:49 +0200

    Update PacketPlayerListHeaderFooterWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/playerlistheaderfooter/PacketPlayerListHeaderFooterWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/playerlistheaderfooter/PacketPlayerListHeaderFooterWrapper.java
@@ -1,56 +1,57 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.playerlistheaderfooter;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketPlayerListHeaderFooterWrapper extends PacketWrapper {
+	
+	Field header;
+	Field footer;
 
-    Field header;
-    Field footer;
+	PacketPlayerListHeaderFooterWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		header = getFirstField(
+			"header",
+			"a"
+		);
+		footer = getFirstField(
+			"footer",
+			"b"
+		);
+	}
 
-    PacketPlayerListHeaderFooterWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	/**
+	 * @return IChatBaseComponent
+	 */
+	public Object getHeader() {
+		return getFieldValue(header);
+	}
+	
+	/**
+	 * @param header: IChatBaseComponent
+	 */
+	public void setHeader(Object header) {
+		setField(this.header, header);
+	}
 
-        header = getFirstField(
-                "header",
-                "a"
-        );
-        footer = getFirstField(
-                "footer",
-                "b"
-        );
-    }
-
-    /**
-     * @return IChatBaseComponent
-     */
-    public Object getHeader() {
-        return getFieldValue(header);
-    }
-
-    /**
-     * @param header: IChatBaseComponent
-     */
-    public void setHeader(Object header) {
-        setField(this.header, header);
-    }
-
-    /**
-     * @return IChatBaseComponent
-     */
-    public Object getFooter() {
-        return getFieldValue(footer);
-    }
-
-    /**
-     * @param footer: IChatBaseComponent
-     */
-    public void setFooter(Object footer) {
-        setField(this.footer, footer);
-    }
+	/**
+	 * @return IChatBaseComponent
+	 */
+	public Object getFooter() {
+		return getFieldValue(footer);
+	}
+	
+	/**
+	 * @param footer: IChatBaseComponent
+	 */
+	public void setFooter(Object footer) {
+		setField(this.footer, footer);
+	}
 }
