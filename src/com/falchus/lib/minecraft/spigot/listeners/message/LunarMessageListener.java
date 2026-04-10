4315a8536084e863d833b34296ad4d924293995a
Elias <elias@falchus.com>
2026-04-10 22:21:07 +0200

    Update LunarMessageListener.java

--- src/com/falchus/lib/minecraft/spigot/listeners/message/LunarMessageListener.java
+++ src/com/falchus/lib/minecraft/spigot/listeners/message/LunarMessageListener.java
@@ -15,39 +15,39 @@ import com.falchus.lib.minecraft.spigot.utils.lunar.LunarJsonPacketUtil;
 import com.google.gson.JsonObject;
 
 public class LunarMessageListener implements Listener {
-
+	
 	private final FalchusLibMinecraftSpigot plugin = FalchusLibMinecraftSpigot.getInstance();
-
+	
 	public LunarMessageListener() {
 		Messenger messenger = Bukkit.getServer().getMessenger();
         messenger.registerIncomingPluginChannel(plugin, "lunar:apollo", (s, player, bytes) -> { });
         messenger.registerIncomingPluginChannel(plugin, "apollo:json", (s, player, bytes) -> { });
         messenger.registerOutgoingPluginChannel(plugin, "apollo:json");
-
+ 
         Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
 
     @EventHandler
     private void onRegisterChannel(PlayerRegisterChannelEvent event) {
         if (!event.getChannel().equalsIgnoreCase("lunar:apollo")) return;
-
+ 
         Player player = event.getPlayer();
-
+ 
         // Sending the player's world name to the client is required for some modules
         LunarJsonPacketUtil.sendPacket(player, createUpdatePlayerWorldMessage(player));
-
+ 
         plugin.getClientManager().set(player, Client.LUNAR);
         Bukkit.getPluginManager().callEvent(new PlayerClientJoinEvent(player, Client.LUNAR));
     }
-
+    
     @EventHandler
     private void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
-
+ 
         // Sending the player's world name to the client is required for some modules
         LunarJsonPacketUtil.sendPacket(player, this.createUpdatePlayerWorldMessage(player));
     }
-
+ 
     private JsonObject createUpdatePlayerWorldMessage(Player player) {
         JsonObject message = new JsonObject();
         message.addProperty("@type", "type.googleapis.com/lunarclient.apollo.player.v1.UpdatePlayerWorldMessage");
