d63b9cb12e0fe4204f085bad7c4ca66222c5da78
Elias <elias@falchus.com>
2026-04-10 22:24:15 +0200

    Update WrappedPacketInAbilities.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/abilities/WrappedPacketInAbilities.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/abilities/WrappedPacketInAbilities.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.abilities;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInAbilities extends PacketAbilitiesWrapper {
 
-    public WrappedPacketInAbilities(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInAbilities",
-                        networkProtocolGame + "PacketPlayInAbilities"
-                )
-        );
-    }
+	public WrappedPacketInAbilities(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInAbilities",
+				networkProtocolGame + "PacketPlayInAbilities"
+			)
+		);
+	}
 }
