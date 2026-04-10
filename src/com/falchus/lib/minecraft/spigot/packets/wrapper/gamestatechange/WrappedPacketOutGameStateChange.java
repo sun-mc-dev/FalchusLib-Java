fbc063cceefae5289b9491de24300c1150647fe5
Elias <elias@falchus.com>
2026-04-10 22:43:45 +0200

    Update WrappedPacketOutGameStateChange.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/gamestatechange/WrappedPacketOutGameStateChange.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/gamestatechange/WrappedPacketOutGameStateChange.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.gamestatechange;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutGameStateChange extends PacketGameStateChangeWrapper {
 
-    WrappedPacketOutGameStateChange(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutGameStateChange",
-                        networkProtocolGame + "PacketPlayOutGameStateChange"
-                )
-        );
-    }
+	WrappedPacketOutGameStateChange(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutGameStateChange",
+				networkProtocolGame + "PacketPlayOutGameStateChange"
+			)
+		);
+	}
 }
