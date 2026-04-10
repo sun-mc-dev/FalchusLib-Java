a3663b81325fdc2045e334e0626cacb4babc8a1e
Elias <elias@falchus.com>
2026-04-10 22:45:06 +0200

    Update PacketHeldItemSlotWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/helditemslot/PacketHeldItemSlotWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/helditemslot/PacketHeldItemSlotWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.helditemslot;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketHeldItemSlotWrapper extends PacketWrapper {
+	
+	Field slot;
 
-    Field slot;
+	PacketHeldItemSlotWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		slot = getFirstField(
+			"slot",
+			"a"
+		);
+	}
 
-    PacketHeldItemSlotWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        slot = getFirstField(
-                "slot",
-                "a"
-        );
-    }
-
-    public int getSlot() {
-        return getFieldValue(slot);
-    }
-
-    public void setSlot(int slot) {
-        setField(this.slot, slot);
-    }
+	public int getSlot() {
+		return getFieldValue(slot);
+	}
+	
+	public void setSlot(int slot) {
+		setField(this.slot, slot);
+	}
 }
