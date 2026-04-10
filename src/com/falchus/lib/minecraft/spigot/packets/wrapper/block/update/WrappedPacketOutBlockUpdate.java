6f230cc741c8c8c8477ea58dd0c65d9a4d787b21
Elias <elias@falchus.com>
2026-04-10 22:33:17 +0200

    Update WrappedPacketOutBlockUpdate.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/update/WrappedPacketOutBlockUpdate.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/update/WrappedPacketOutBlockUpdate.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.block.update;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutBlockUpdate extends PacketBlockUpdateWrapper {
 
-    public WrappedPacketOutBlockUpdate(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutBlockChange",
-                        networkProtocolGame + "PacketPlayOutBlockChange"
-                )
-        );
-    }
+	public WrappedPacketOutBlockUpdate(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutBlockChange",
+				networkProtocolGame + "PacketPlayOutBlockChange"
+			)
+		);
+	}
 }
