45927ff5fbdc7e085fae37d74482c2c2a1efe7f6
Elias <elias@falchus.com>
2026-04-10 22:54:18 +0200

    Update PacketScoreboardScoreWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/score/PacketScoreboardScoreWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/scoreboard/score/PacketScoreboardScoreWrapper.java
@@ -1,57 +1,58 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.scoreboard.score;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketScoreboardScoreWrapper extends PacketWrapper {
+	
+	Field owner;
+	Field objectiveName;
+	Field score;
 
-    Field owner;
-    Field objectiveName;
-    Field score;
+	PacketScoreboardScoreWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		owner = getFirstField(
+			"owner",
+			"a"
+		);
+		objectiveName = getFirstField(
+			"objectiveName",
+			"b"
+		);
+		score = getFirstField(
+			"score",
+			"c"
+		);
+	}
 
-    PacketScoreboardScoreWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public String getOwner() {
+		return getFieldValue(owner);
+	}
+	
+	public void setOwner(String owner) {
+		setField(this.owner, owner);
+	}
 
-        owner = getFirstField(
-                "owner",
-                "a"
-        );
-        objectiveName = getFirstField(
-                "objectiveName",
-                "b"
-        );
-        score = getFirstField(
-                "score",
-                "c"
-        );
-    }
+	public String getObjectiveName() {
+		return getFieldValue(objectiveName);
+	}
+	
+	public void setObjectiveName(String objectiveName) {
+		setField(this.objectiveName, objectiveName);
+	}
 
-    public String getOwner() {
-        return getFieldValue(owner);
-    }
-
-    public void setOwner(String owner) {
-        setField(this.owner, owner);
-    }
-
-    public String getObjectiveName() {
-        return getFieldValue(objectiveName);
-    }
-
-    public void setObjectiveName(String objectiveName) {
-        setField(this.objectiveName, objectiveName);
-    }
-
-    public int getScore() {
-        return getFieldValue(score);
-    }
-
-    public void setScore(int score) {
-        setField(this.score, score);
-    }
+	public int getScore() {
+		return getFieldValue(score);
+	}
+	
+	public void setScore(int score) {
+		setField(this.score, score);
+	}
 }
