4dc5e598cad0d41c5f45aa28aec117814aef2a63
Elias <elias@falchus.com>
2026-04-10 23:07:13 +0200

    Update PacketWindowDataWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/window/data/PacketWindowDataWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/window/data/PacketWindowDataWrapper.java
@@ -1,57 +1,58 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.window.data;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketWindowDataWrapper extends PacketWrapper {
+	
+	Field containerId;
+	Field id;
+	Field value;
 
-    Field containerId;
-    Field id;
-    Field value;
+	PacketWindowDataWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		containerId = getFirstField(
+			"containerId",
+			"a"
+		);
+		id = getFirstField(
+			"id",
+			"b"
+		);
+		value = getFirstField(
+			"value",
+			"c"
+		);
+	}
 
-    PacketWindowDataWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getContainerId() {
+		return getFieldValue(containerId);
+	}
+	
+	public void setContainerId(int containerId) {
+		setField(this.containerId, containerId);
+	}
 
-        containerId = getFirstField(
-                "containerId",
-                "a"
-        );
-        id = getFirstField(
-                "id",
-                "b"
-        );
-        value = getFirstField(
-                "value",
-                "c"
-        );
-    }
+	public int getId() {
+		return getFieldValue(id);
+	}
+	
+	public void setId(int id) {
+		setField(this.id, id);
+	}
 
-    public int getContainerId() {
-        return getFieldValue(containerId);
-    }
-
-    public void setContainerId(int containerId) {
-        setField(this.containerId, containerId);
-    }
-
-    public int getId() {
-        return getFieldValue(id);
-    }
-
-    public void setId(int id) {
-        setField(this.id, id);
-    }
-
-    public int getValue() {
-        return getFieldValue(value);
-    }
-
-    public void setValue(int value) {
-        setField(this.value, value);
-    }
+	public int getValue() {
+		return getFieldValue(value);
+	}
+	
+	public void setValue(int value) {
+		setField(this.value, value);
+	}
 }
