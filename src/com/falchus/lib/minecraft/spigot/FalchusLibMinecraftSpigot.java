188c378fc66cfce9f7636accc9b6765c593d239f
Elias <elias@falchus.com>
2026-04-10 23:19:05 +0200

    Update FalchusLibMinecraftSpigot.java

--- src/com/falchus/lib/minecraft/spigot/FalchusLibMinecraftSpigot.java
+++ src/com/falchus/lib/minecraft/spigot/FalchusLibMinecraftSpigot.java
@@ -1,52 +1,45 @@
 package com.falchus.lib.minecraft.spigot;
 
-import com.falchus.lib.minecraft.FalchusLibMinecraft;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import com.falchus.lib.minecraft.spigot.listeners.*;
-import com.falchus.lib.minecraft.spigot.listeners.message.LabyModMessageListener;
-import com.falchus.lib.minecraft.spigot.listeners.message.LunarMessageListener;
-import com.falchus.lib.minecraft.spigot.manager.ClientManager;
-import com.falchus.lib.minecraft.spigot.utils.Metrics;
-import com.falchus.lib.minecraft.spigot.utils.SchedulerUtils;
+import com.falchus.lib.minecraft.spigot.listeners.message.*;
+import com.falchus.lib.minecraft.spigot.manager.*;
+import com.falchus.lib.minecraft.spigot.utils.*;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
-import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FalchusLibMinecraftSpigot extends JavaPlugin {
 
-    @Getter
-    static FalchusLibMinecraftSpigot instance;
-
-    LabyModMessageListener labyModMessageListener;
-    LunarMessageListener lunarMessageListener;
-    EntityPlayerListener entityPlayerListener;
-    FreezeListener freezeListener;
-    ItemListener itemListener;
-    JoinQuitListener joinQuitListener;
-    LobbyCancelListener lobbyCancelListener;
-    ClientManager clientManager;
-
-    @Override
-    public void onEnable() {
-        instance = this;
-
-        if (FalchusLibMinecraft.isFolia()) {
-            getLogger().info("Folia detected — using Folia-compatible scheduler.");
-        }
-
-        new Metrics(this, 28050);
-
-        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-
-        labyModMessageListener = new LabyModMessageListener();
-        lunarMessageListener = new LunarMessageListener();
-        entityPlayerListener = new EntityPlayerListener();
-        freezeListener = new FreezeListener();
-        itemListener = new ItemListener();
-        joinQuitListener = new JoinQuitListener();
-        lobbyCancelListener = new LobbyCancelListener();
-        clientManager = new ClientManager();
-    }
+	@Getter static FalchusLibMinecraftSpigot instance;
+	
+	LabyModMessageListener labyModMessageListener;
+	LunarMessageListener lunarMessageListener;
+	EntityPlayerListener entityPlayerListener;
+	FreezeListener freezeListener;
+	ItemListener itemListener;
+	JoinQuitListener joinQuitListener;
+	LobbyCancelListener lobbyCancelListener;
+	ClientManager clientManager;
+	
+	@Override
+	public void onEnable() {
+		instance = this;
+		new Metrics(this, 28050);
+		
+		getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+		
+		labyModMessageListener = new LabyModMessageListener();
+		lunarMessageListener = new LunarMessageListener();
+		entityPlayerListener = new EntityPlayerListener();
+		freezeListener = new FreezeListener();
+		itemListener = new ItemListener();
+		joinQuitListener = new JoinQuitListener();
+		lobbyCancelListener = new LobbyCancelListener();
+		clientManager = new ClientManager();
+	}
 }
