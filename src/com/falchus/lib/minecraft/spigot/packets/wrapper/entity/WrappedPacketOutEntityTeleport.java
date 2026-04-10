deb8091bd3eeaa7c968e1eee735e5269671c8a6f
Elias <elias@falchus.com>
2026-04-10 22:40:58 +0200

    Update WrappedPacketOutEntityTeleport.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityTeleport.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/entity/WrappedPacketOutEntityTeleport.java
@@ -1,47 +1,47 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.entity;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketOutEntityTeleport extends PacketEntityWrapper {
 
-    Field id;
-    Field onGround;
+	Field id;
+	Field onGround;
+	
+	WrappedPacketOutEntityTeleport(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutEntityTeleport",
+				networkProtocolGame + "PacketPlayOutEntityTeleport"
+			)
+		);
+		
+		id = getFirstField(
+			"id",
+			"a"
+		);
+		onGround = getFirstField(
+			"onGround",
+			"g"
+		);
+	}
 
-    WrappedPacketOutEntityTeleport(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutEntityTeleport",
-                        networkProtocolGame + "PacketPlayOutEntityTeleport"
-                )
-        );
+	public int getId() {
+		return getFieldValue(id);
+	}
+	
+	public void setId(int id) {
+		setField(this.id, id);
+	}
 
-        id = getFirstField(
-                "id",
-                "a"
-        );
-        onGround = getFirstField(
-                "onGround",
-                "g"
-        );
-    }
-
-    public int getId() {
-        return getFieldValue(id);
-    }
-
-    public void setId(int id) {
-        setField(this.id, id);
-    }
-
-    public boolean isOnGround() {
-        return getFieldValue(onGround);
-    }
-
-    public void setOnGround(boolean onGround) {
-        setField(this.onGround, onGround);
-    }
+	public boolean isOnGround() {
+		return getFieldValue(onGround);
+	}
+	
+	public void setOnGround(boolean onGround) {
+		setField(this.onGround, onGround);
+	}
 }
