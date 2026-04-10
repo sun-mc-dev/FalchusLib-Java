f3c89d1a1de2a87301f853dee9323a5198efeacd
Elias <elias@falchus.com>
2026-04-10 23:01:54 +0200

    Update WrappedPacketOutAnimationTitle.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/title/WrappedPacketOutAnimationTitle.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/title/WrappedPacketOutAnimationTitle.java
@@ -1,63 +1,63 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.title;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 public class WrappedPacketOutAnimationTitle extends PacketTitleWrapper {
 
-    Field fadeInTime;
-    Field stayTime;
-    Field fadeOutTime;
+	Field fadeInTime;
+	Field stayTime;
+	Field fadeOutTime;
+	
+	WrappedPacketOutAnimationTitle(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutTitle",
+				networkProtocolGame + "ClientboundSetTitlesAnimationPacket"
+			)
+		);
+		
+		fadeInTime = getFirstField(
+			"fadeInTime",
+			"fadeIn",
+			"c"
+		);
+		stayTime = getFirstField(
+			"stayTime",
+			"stay",
+			"d"
+		);
+		fadeOutTime = getFirstField(
+			"fadeOutTime",
+			"fadeOut",
+			"e"
+		);
+	}
 
-    WrappedPacketOutAnimationTitle(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutTitle",
-                        networkProtocolGame + "ClientboundSetTitlesAnimationPacket"
-                )
-        );
+	public int getFadeInTime() {
+		return getFieldValue(fadeInTime);
+	}
+	
+	public void setFadeInTime(int fadeInTime) {
+		setField(this.fadeInTime, fadeInTime);
+	}
 
-        fadeInTime = getFirstField(
-                "fadeInTime",
-                "fadeIn",
-                "c"
-        );
-        stayTime = getFirstField(
-                "stayTime",
-                "stay",
-                "d"
-        );
-        fadeOutTime = getFirstField(
-                "fadeOutTime",
-                "fadeOut",
-                "e"
-        );
-    }
+	public int getStayTime() {
+		return getFieldValue(stayTime);
+	}
+	
+	public void setStayTime(int stayTime) {
+		setField(this.stayTime, stayTime);
+	}
 
-    public int getFadeInTime() {
-        return getFieldValue(fadeInTime);
-    }
-
-    public void setFadeInTime(int fadeInTime) {
-        setField(this.fadeInTime, fadeInTime);
-    }
-
-    public int getStayTime() {
-        return getFieldValue(stayTime);
-    }
-
-    public void setStayTime(int stayTime) {
-        setField(this.stayTime, stayTime);
-    }
-
-    public int getFadeOutTime() {
-        return getFieldValue(fadeOutTime);
-    }
-
-    public void setFadeOutTime(int fadeOutTime) {
-        setField(this.fadeOutTime, fadeOutTime);
-    }
+	public int getFadeOutTime() {
+		return getFieldValue(fadeOutTime);
+	}
+	
+	public void setFadeOutTime(int fadeOutTime) {
+		setField(this.fadeOutTime, fadeOutTime);
+	}
 }
