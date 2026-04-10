66612596e4c9dbb9030ffafc634d589d0860da00
Elias <elias@falchus.com>
2026-04-10 23:20:58 +0200

    Update JsonStorage.java

--- src/com/falchus/lib/storage/impl/json/JsonStorage.java
+++ src/com/falchus/lib/storage/impl/json/JsonStorage.java
@@ -1,13 +1,13 @@
 package com.falchus.lib.storage.impl.json;
 
+import java.nio.file.Path;
+
 import com.falchus.lib.storage.Storage;
 import com.falchus.lib.storage.serializer.Serializer;
 
-import java.nio.file.Path;
-
 class JsonStorage extends Storage {
 
-    JsonStorage(Serializer<?> serializer, Path folder, String fileName, String defaultContent) {
-        super(serializer, folder, fileName + ".json", defaultContent);
-    }
+	JsonStorage(Serializer<?> serializer, Path folder, String fileName, String defaultContent) {
+		super(serializer, folder, fileName + ".json", defaultContent);
+	}
 }
