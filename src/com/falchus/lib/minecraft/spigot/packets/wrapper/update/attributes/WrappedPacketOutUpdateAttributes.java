52e4b51c917e9ce174eb4e0250fbd3570d9d4b32
Elias <elias@falchus.com>
2026-04-10 23:02:49 +0200

    Update WrappedPacketOutUpdateAttributes.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/attributes/WrappedPacketOutUpdateAttributes.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/attributes/WrappedPacketOutUpdateAttributes.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.attributes;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutUpdateAttributes extends PacketUpdateAttributesWrapper {
 
-    public WrappedPacketOutUpdateAttributes(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutUpdateAttributes",
-                        networkProtocolGame + "PacketPlayOutUpdateAttributes"
-                )
-        );
-    }
+	public WrappedPacketOutUpdateAttributes(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutUpdateAttributes",
+				networkProtocolGame + "PacketPlayOutUpdateAttributes"
+			)
+		);
+	}
 }
