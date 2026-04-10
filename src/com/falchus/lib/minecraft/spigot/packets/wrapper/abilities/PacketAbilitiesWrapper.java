d85e8e2cdf9cfaec3abe567c158252821c407e6f
Elias <elias@falchus.com>
2026-04-10 22:23:57 +0200

    Update PacketAbilitiesWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/abilities/PacketAbilitiesWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/abilities/PacketAbilitiesWrapper.java
@@ -1,31 +1,32 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.abilities;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketAbilitiesWrapper extends PacketWrapper {
+	
+	Field flying;
 
-    Field flying;
+	PacketAbilitiesWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		flying = getFirstField(
+			"isFlying",
+			"b"
+		);
+	}
 
-    PacketAbilitiesWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        flying = getFirstField(
-                "isFlying",
-                "b"
-        );
-    }
-
-    public boolean isFlying() {
-        return getFieldValue(flying);
-    }
-
-    public void setFlying(boolean isFlying) {
-        setField(this.flying, isFlying);
-    }
+	public boolean isFlying() {
+		return getFieldValue(flying);
+	}
+	
+	public void setFlying(boolean isFlying) {
+		setField(this.flying, isFlying);
+	}
 }
