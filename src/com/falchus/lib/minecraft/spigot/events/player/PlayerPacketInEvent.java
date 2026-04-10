62cc41bb2cbd7cf3600a436bb7c8314763bf0d3e
Elias <elias@falchus.com>
2026-04-10 22:20:10 +0200

    Update PlayerPacketInEvent.java

--- src/com/falchus/lib/minecraft/spigot/events/player/PlayerPacketInEvent.java
+++ src/com/falchus/lib/minecraft/spigot/events/player/PlayerPacketInEvent.java
@@ -1,16 +1,18 @@
 package com.falchus.lib.minecraft.spigot.events.player;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.Getter;
 import org.bukkit.entity.Player;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.Getter;
 
 /**
  * Called when a player receives a packet.
  */
 @Getter
 public class PlayerPacketInEvent extends PlayerPacketEvent {
-
-    public PlayerPacketInEvent(boolean async, Player player, PacketWrapper packet) {
-        super(async, player, packet);
-    }
+	
+	public PlayerPacketInEvent(boolean async, Player player, PacketWrapper packet) {
+		super(async, player, packet);
+	}
 }
