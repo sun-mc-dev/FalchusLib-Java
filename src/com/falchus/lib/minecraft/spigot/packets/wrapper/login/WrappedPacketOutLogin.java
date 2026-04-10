675b6053bd8fe44ad8e168119ef2163febfe8820
Elias <elias@falchus.com>
2026-04-10 22:46:37 +0200

    Update WrappedPacketOutLogin.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/login/WrappedPacketOutLogin.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/login/WrappedPacketOutLogin.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.login;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutLogin extends PacketLoginWrapper {
 
-    WrappedPacketOutLogin(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutLogin",
-                        networkProtocolGame + "PacketPlayOutLogin"
-                )
-        );
-    }
+	WrappedPacketOutLogin(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutLogin",
+				networkProtocolGame + "PacketPlayOutLogin"
+			)
+		);
+	}
 }
