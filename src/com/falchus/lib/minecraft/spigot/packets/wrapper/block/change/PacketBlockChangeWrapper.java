408b5b380a74d8be97d3ffe55ea6e1e66ab4f47b
Elias <elias@falchus.com>
2026-04-10 22:29:30 +0200

    Update PacketBlockChangeWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/change/PacketBlockChangeWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/block/change/PacketBlockChangeWrapper.java
@@ -10,29 +10,29 @@ import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(makeFinal = true)
 abstract class PacketBlockChangeWrapper extends PacketWrapper {
+	
+	Field pos;
+	
+	PacketBlockChangeWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		pos = getFirstField(
+			"pos",
+			"a"
+		);
+	}
 
-    Field pos;
-
-    PacketBlockChangeWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        pos = getFirstField(
-                "pos",
-                "a"
-        );
-    }
-
-    /**
-     * @return BlockPosition
-     */
-    public Object getPos() {
-        return getFieldValue(pos);
-    }
-
-    /**
-     * @param pos: BlockPosition
-     */
-    public void setPos(Object pos) {
-        setField(this.pos, pos);
-    }
+	/**
+	 * @return BlockPosition
+	 */
+	public Object getPos() {
+		return getFieldValue(pos);
+	}
+	
+	/**
+	 * @param pos: BlockPosition
+	 */
+	public void setPos(Object pos) {
+		setField(this.pos, pos);
+	}
 }
