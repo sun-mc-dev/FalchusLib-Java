0ac83a6fec7db3d55ffe4b6c4fc833817121cd55
Elias <elias@falchus.com>
2026-04-10 23:05:01 +0200

    Update PacketUpdateWindowData.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/window/PacketUpdateWindowData.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/update/window/PacketUpdateWindowData.java
@@ -1,43 +1,43 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.update.window;
 
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketUpdateWindowData extends PacketUpdateWindow {
+	
+	Field id;
+	Field value;
 
-    Field id;
-    Field value;
+	PacketUpdateWindowData(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		id = getFirstField(
+			"id",
+			"b"
+		);
+		value = getFirstField(
+			"value",
+			"c"
+		);
+	}
 
-    PacketUpdateWindowData(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getId() {
+		return getFieldValue(id);
+	}
+	
+	public void setId(int id) {
+		setField(this.id, id);
+	}
 
-        id = getFirstField(
-                "id",
-                "b"
-        );
-        value = getFirstField(
-                "value",
-                "c"
-        );
-    }
-
-    public int getId() {
-        return getFieldValue(id);
-    }
-
-    public void setId(int id) {
-        setField(this.id, id);
-    }
-
-    public int getValue() {
-        return getFieldValue(value);
-    }
-
-    public void setValue(int value) {
-        setField(this.value, value);
-    }
+	public int getValue() {
+		return getFieldValue(value);
+	}
+	
+	public void setValue(int value) {
+		setField(this.value, value);
+	}
 }
