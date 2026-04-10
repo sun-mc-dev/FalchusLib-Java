a25a81e7e0ccfff6c99929f0d8ebb7fb488dedde
Elias <elias@falchus.com>
2026-04-10 22:26:35 +0200

    Update WrappedPacketInArmAnimation.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/armanimation/WrappedPacketInArmAnimation.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/armanimation/WrappedPacketInArmAnimation.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.armanimation;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInArmAnimation extends PacketArmAnimationWrapper {
 
-    public WrappedPacketInArmAnimation(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInArmAnimation",
-                        networkProtocolGame + "PacketPlayInArmAnimation"
-                )
-        );
-    }
+	public WrappedPacketInArmAnimation(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInArmAnimation",
+				networkProtocolGame + "PacketPlayInArmAnimation"
+			)
+		);
+	}
 }
