49bb5c9b05389a37093c7f16ff095d8fd64fe577
Elias <elias@falchus.com>
2026-04-10 22:58:00 +0200

    Update PacketSpawnPositionWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/position/PacketSpawnPositionWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/spawn/position/PacketSpawnPositionWrapper.java
@@ -1,13 +1,14 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.spawn.position;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-
 import java.util.Set;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
 
 abstract class PacketSpawnPositionWrapper extends PacketWrapper {
 
-    PacketSpawnPositionWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-    }
+	PacketSpawnPositionWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+	}
 }
