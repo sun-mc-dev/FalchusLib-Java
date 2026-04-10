01e4adc810bd279ce3298c3ea819df5ef8af4a77
Elias <elias@falchus.com>
2026-04-10 22:45:19 +0200

    Update WrappedPacketInHeldItemSlot.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/helditemslot/WrappedPacketInHeldItemSlot.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/helditemslot/WrappedPacketInHeldItemSlot.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.helditemslot;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInHeldItemSlot extends PacketHeldItemSlotWrapper {
 
-    public WrappedPacketInHeldItemSlot(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInHeldItemSlot",
-                        networkProtocolGame + "PacketPlayInHeldItemSlot"
-                )
-        );
-    }
+	public WrappedPacketInHeldItemSlot(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInHeldItemSlot",
+				networkProtocolGame + "PacketPlayInHeldItemSlot"
+			)
+		);
+	}
 }
