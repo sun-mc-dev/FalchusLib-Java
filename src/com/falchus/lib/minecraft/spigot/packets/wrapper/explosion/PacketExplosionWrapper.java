6b8140ecad5b0af48c0b7aad7f02755c2c189866
Elias <elias@falchus.com>
2026-04-10 22:42:01 +0200

    Update PacketExplosionWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/explosion/PacketExplosionWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/explosion/PacketExplosionWrapper.java
@@ -1,32 +1,33 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.explosion;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketExplosionWrapper extends PacketWrapper {
+	
+	Field radius;
 
-    Field radius;
+	PacketExplosionWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		radius = getFirstField(
+			"radius",
+			"power",
+			"d"
+		);
+	}
 
-    PacketExplosionWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        radius = getFirstField(
-                "radius",
-                "power",
-                "d"
-        );
-    }
-
-    public float getRadius() {
-        return getFieldValue(radius);
-    }
-
-    public void setRadius(float radius) {
-        setField(this.radius, radius);
-    }
+	public float getRadius() {
+		return getFieldValue(radius);
+	}
+	
+	public void setRadius(float radius) {
+		setField(this.radius, radius);
+	}
 }
