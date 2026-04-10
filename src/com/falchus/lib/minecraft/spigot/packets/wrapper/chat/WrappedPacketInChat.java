a740d0ea9c5e3f1e878478b36290eccc7fb7e30e
Elias <elias@falchus.com>
2026-04-10 22:34:26 +0200

    Update WrappedPacketInChat.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/chat/WrappedPacketInChat.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/chat/WrappedPacketInChat.java
@@ -1,26 +1,26 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.chat;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInChat extends PacketChatWrapper {
 
-    public WrappedPacketInChat(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInChat",
-                        networkProtocolGame + "ServerboundChatPacket"
-                )
-        );
-    }
+	public WrappedPacketInChat(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInChat",
+				networkProtocolGame + "ServerboundChatPacket"
+			)
+		);
+	}
 
-    @Override
-    public String getMessage() {
-        return (String) super.getMessage();
-    }
-
-    @Override
-    public void setMessage(String message) {
-        setField(this.message, message);
-    }
+	@Override
+	public String getMessage() {
+		return (String) super.getMessage();
+	}
+	
+	@Override
+	public void setMessage(String message) {
+		setField(this.message, message);
+	}
 }
