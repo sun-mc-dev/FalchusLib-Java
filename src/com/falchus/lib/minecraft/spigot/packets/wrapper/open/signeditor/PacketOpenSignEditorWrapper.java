cdd972f7175044418711d32fec2ccb168b4bbd24
Elias <elias@falchus.com>
2026-04-10 22:48:43 +0200

    Update PacketOpenSignEditorWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/open/signeditor/PacketOpenSignEditorWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/open/signeditor/PacketOpenSignEditorWrapper.java
@@ -1,37 +1,38 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.open.signeditor;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketOpenSignEditorWrapper extends PacketWrapper {
+	
+	Field pos;
 
-    Field pos;
+	PacketOpenSignEditorWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		pos = getFirstField(
+			"pos",
+			"a"
+		);
+	}
 
-    PacketOpenSignEditorWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        pos = getFirstField(
-                "pos",
-                "a"
-        );
-    }
-
-    /**
-     * @return BlockPosition
-     */
-    public Object getPos() {
-        return getFieldValue(pos);
-    }
-
-    /**
-     * @param pos: BlockPosition
-     */
-    public void setPos(Object pos) {
-        setField(this.pos, pos);
-    }
+	/**
+	 * @return BlockPosition
+	 */
+	public Object getPos() {
+		return getFieldValue(pos);
+	}
+	
+	/**
+	 * @param pos: BlockPosition
+	 */
+	public void setPos(Object pos) {
+		setField(this.pos, pos);
+	}
 }
