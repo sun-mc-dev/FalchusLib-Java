f150e136aa48daf28ac8f47c0cca86e941356ac8
Elias <elias@falchus.com>
2026-04-10 22:14:43 +0200

    Update TriConsumer.java

--- src/com/falchus/lib/interfaces/consumer/TriConsumer.java
+++ src/com/falchus/lib/interfaces/consumer/TriConsumer.java
@@ -5,17 +5,17 @@ import java.util.function.BiConsumer;
 /**
  * A functional interface similar to {@link BiConsumer},
  * but accepts three input arguments instead of two.
- *
+ * 
  * @param <T> the type of the first argument
  * @param <U> the type of the second argument
  * @param <V> the type of the third argument
  */
 @FunctionalInterface
 public interface TriConsumer<T, U, V> {
-
+	
 	/**
 	 * Performs this operation on the given arguments.
-	 *
+	 * 
 	 * @param t the first input argument
 	 * @param u the second input argument
 	 * @param v the third input argument
