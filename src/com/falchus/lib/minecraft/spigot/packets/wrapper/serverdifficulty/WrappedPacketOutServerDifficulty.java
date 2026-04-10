519bbcbf16102480f38c62b241f0dfeb4cfa4a26
Elias <elias@falchus.com>
2026-04-10 22:55:43 +0200

    Update WrappedPacketOutServerDifficulty.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/serverdifficulty/WrappedPacketOutServerDifficulty.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/serverdifficulty/WrappedPacketOutServerDifficulty.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.serverdifficulty;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutServerDifficulty extends PacketServerDifficultyWrapper {
 
-    WrappedPacketOutServerDifficulty(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutServerDifficulty",
-                        networkProtocolGame + "PacketPlayOutServerDifficulty"
-                )
-        );
-    }
+	WrappedPacketOutServerDifficulty(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutServerDifficulty",
+				networkProtocolGame + "PacketPlayOutServerDifficulty"
+			)
+		);
+	}
 }
