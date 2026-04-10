fe8248dbb4745d160bb8849b9b5b453c9a67604f
Elias <elias@falchus.com>
2026-04-10 22:59:02 +0200

    Update WrappedPacketInSpectate.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/spectate/WrappedPacketInSpectate.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/spectate/WrappedPacketInSpectate.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.spectate;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketInSpectate extends PacketSpectateWrapper {
 
-    public WrappedPacketInSpectate(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayInSpectate",
-                        networkProtocolGame + "PacketPlayInSpectate"
-                )
-        );
-    }
+	public WrappedPacketInSpectate(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayInSpectate",
+				networkProtocolGame + "PacketPlayInSpectate"
+			)
+		);
+	}
 }
