ea7534101c1130178a5b7c0846ffaf880954e10e
Elias <elias@falchus.com>
2026-04-10 23:20:46 +0200

    Update JsonObjectStorage.java

--- src/com/falchus/lib/storage/impl/json/JsonObjectStorage.java
+++ src/com/falchus/lib/storage/impl/json/JsonObjectStorage.java
@@ -1,13 +1,14 @@
 package com.falchus.lib.storage.impl.json;
 
-import com.falchus.lib.storage.serializer.Serializer;
+import java.nio.file.Path;
+
 import org.json.simple.JSONObject;
 
-import java.nio.file.Path;
+import com.falchus.lib.storage.serializer.Serializer;
 
 public class JsonObjectStorage extends JsonStorage {
 
-    public JsonObjectStorage(Serializer<?> serializer, Path folder, String fileName) {
-        super(serializer, folder, fileName, new JSONObject().toJSONString());
-    }
+	public JsonObjectStorage(Serializer<?> serializer, Path folder, String fileName) {
+		super(serializer, folder, fileName, new JSONObject().toJSONString());
+	}
 }
