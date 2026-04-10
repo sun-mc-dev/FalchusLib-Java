b113cbf66928d4d6bfc3e041acfa05afb33e3a12
Elias <elias@falchus.com>
2026-04-10 22:05:59 +0200

    Update PlayerPacketEvent.java

--- src/com/falchus/lib/minecraft/spigot/events/player/PlayerPacketEvent.java
+++ src/com/falchus/lib/minecraft/spigot/events/player/PlayerPacketEvent.java
@@ -8,18 +8,22 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Called when a player receives/sends a packet.
  */
 @Getter
 public class PlayerPacketEvent extends Event implements Cancellable {
 
-    @Getter
-    private static final HandlerList handlerList = new HandlerList();
+    @Getter private static final HandlerList handlerList = new HandlerList();
+    @Setter private boolean cancelled;
+    
     private final Player player;
     private final PacketWrapper packet;
-    @Setter
-    private boolean cancelled;
 
     public PlayerPacketEvent(boolean async, Player player, PacketWrapper packet) {
         super(async);
