5659fc8438bccce77b8c189ac644a16df0fcdc27
Elias <elias@falchus.com>
2026-04-10 23:08:25 +0200

    Update PacketWorldEventWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/world/event/PacketWorldEventWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/world/event/PacketWorldEventWrapper.java
@@ -1,77 +1,78 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.world.event;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
+
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 // TODO: add PacketPlayOutWorldParticles wrapper
 @FieldDefaults(makeFinal = true)
 abstract class PacketWorldEventWrapper extends PacketWrapper {
+	
+	Field type;
+	Field pos;
+	Field data;
+	Field globalEvent;
 
-    Field type;
-    Field pos;
-    Field data;
-    Field globalEvent;
+	PacketWorldEventWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		type = getFirstField(
+			"type",
+			"a"
+		);
+		pos = getFirstField(
+			"pos",
+			"b"
+		);
+		data = getFirstField(
+			"data",
+			"c"
+		);
+		globalEvent = getFirstField(
+			"globalEvent",
+			"d"
+		);
+	}
 
-    PacketWorldEventWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getType() {
+		return getFieldValue(type);
+	}
+	
+	public void setType(int type) {
+		setField(this.type, type);
+	}
 
-        type = getFirstField(
-                "type",
-                "a"
-        );
-        pos = getFirstField(
-                "pos",
-                "b"
-        );
-        data = getFirstField(
-                "data",
-                "c"
-        );
-        globalEvent = getFirstField(
-                "globalEvent",
-                "d"
-        );
-    }
+	/**
+	 * @return BlockPosition
+	 */
+	public Object getPos() {
+		return getFieldValue(type);
+	}
+	
+	/**
+	 * @param pos: BlockPosition
+	 */
+	public void setPos(Object pos) {
+		setField(this.pos, pos);
+	}
 
-    public int getType() {
-        return getFieldValue(type);
-    }
+	public int getData() {
+		return getFieldValue(data);
+	}
+	
+	public void setData(int data) {
+		setField(this.data, data);
+	}
 
-    public void setType(int type) {
-        setField(this.type, type);
-    }
-
-    /**
-     * @return BlockPosition
-     */
-    public Object getPos() {
-        return getFieldValue(type);
-    }
-
-    /**
-     * @param pos: BlockPosition
-     */
-    public void setPos(Object pos) {
-        setField(this.pos, pos);
-    }
-
-    public int getData() {
-        return getFieldValue(data);
-    }
-
-    public void setData(int data) {
-        setField(this.data, data);
-    }
-
-    public boolean isGlobalEvent() {
-        return getFieldValue(globalEvent);
-    }
-
-    public void setGlobalEvent(boolean globalEvent) {
-        setField(this.globalEvent, globalEvent);
-    }
+	public boolean isGlobalEvent() {
+		return getFieldValue(globalEvent);
+	}
+	
+	public void setGlobalEvent(boolean globalEvent) {
+		setField(this.globalEvent, globalEvent);
+	}
 }
