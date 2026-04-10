24bc93487fc00e131a24e91458a4ab571aa40e62
Elias <elias@falchus.com>
2026-04-10 22:20:54 +0200

    Update LabyModMessageListener.java

--- src/com/falchus/lib/minecraft/spigot/listeners/message/LabyModMessageListener.java
+++ src/com/falchus/lib/minecraft/spigot/listeners/message/LabyModMessageListener.java
@@ -13,9 +13,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
 public class LabyModMessageListener implements PluginMessageListener {
-
+	
 	private final FalchusLibMinecraftSpigot plugin = FalchusLibMinecraftSpigot.getInstance();
-
+	
 	public LabyModMessageListener() {
 		plugin.getServer().getMessenger().registerIncomingPluginChannel(plugin, "labymod3:main", this);
 		plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "labymod3:main");
@@ -24,10 +24,10 @@ public class LabyModMessageListener implements PluginMessageListener {
 	@Override
 	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
 		if (!channel.equals("labymod3:main")) return;
-
+		
 		ByteBuf buf = Unpooled.wrappedBuffer(message);
 		String key = LabyModProtocol.readString(buf, Short.MAX_VALUE);
-
+		
 		// LabyMod user joins the server
 		if (key.equals("INFO")) {
 			plugin.getClientManager().set(player, Client.LABYMOD);
