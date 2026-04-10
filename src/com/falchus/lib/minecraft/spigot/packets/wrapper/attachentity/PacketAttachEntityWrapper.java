85abe2f4bd9b3f41ed6876ddf2fc5abc52c9fa8a
Elias <elias@falchus.com>
2026-04-10 22:27:03 +0200

    Update PacketAttachEntityWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/attachentity/PacketAttachEntityWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/attachentity/PacketAttachEntityWrapper.java
@@ -1,44 +1,45 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.attachentity;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketAttachEntityWrapper extends PacketWrapper {
+	
+	Field sourceId;
+	Field destId;
 
-    Field sourceId;
-    Field destId;
+	PacketAttachEntityWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		sourceId = getFirstField(
+			"sourceId",
+			"b"
+		);
+		destId = getFirstField(
+			"destId",
+			"c"
+		);
+	}
 
-    PacketAttachEntityWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
+	public int getSourceId() {
+		return getFieldValue(sourceId);
+	}
+	
+	public void setSourceId(int sourceId) {
+		setField(this.sourceId, sourceId);
+	}
 
-        sourceId = getFirstField(
-                "sourceId",
-                "b"
-        );
-        destId = getFirstField(
-                "destId",
-                "c"
-        );
-    }
-
-    public int getSourceId() {
-        return getFieldValue(sourceId);
-    }
-
-    public void setSourceId(int sourceId) {
-        setField(this.sourceId, sourceId);
-    }
-
-    public int getDestId() {
-        return getFieldValue(destId);
-    }
-
-    public void setDestId(int destId) {
-        setField(this.destId, destId);
-    }
+	public int getDestId() {
+		return getFieldValue(destId);
+	}
+	
+	public void setDestId(int destId) {
+		setField(this.destId, destId);
+	}
 }
