c1bd00163c73646c6893f3fb1810641a3b8fa8d1
Elias <elias@falchus.com>
2026-04-10 23:22:15 +0200

    Update CountdownTask.java

--- src/com/falchus/lib/task/impl/CountdownTask.java
+++ src/com/falchus/lib/task/impl/CountdownTask.java
@@ -1,6 +1,7 @@
 package com.falchus.lib.task.impl;
 
 import com.falchus.lib.task.Task;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,19 +11,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public abstract class CountdownTask extends Task {
-
-    private int remaining;
-
-    @Override
-    public final void onRun(int tick) {
-        if (remaining <= 0) {
-            end();
-            return;
-        }
-        onCountdown(remaining);
-        remaining--;
-    }
-
-    protected void onCountdown(int remaining) {
-    }
+	
+	private int remaining;
+	
+	@Override
+	public final void onRun(int tick) {		
+		if (remaining <= 0) {
+			end();
+			return;
+		}
+		onCountdown(remaining);
+		remaining--;
+	}
+	
+	protected void onCountdown(int remaining) {}
 }
