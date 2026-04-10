c6a238c309d3b58616e7f2effe7456e8abb23b55
Elias <elias@falchus.com>
2026-04-10 22:56:47 +0200

    Update WrappedPacketOutSetSlot.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/set/slot/WrappedPacketOutSetSlot.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/set/slot/WrappedPacketOutSetSlot.java
@@ -1,16 +1,16 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.set.slot;
 
-import lombok.NonNull;
-
 import java.util.Set;
+
+import lombok.NonNull;
 
 public class WrappedPacketOutSetSlot extends PacketSetSlotWrapper {
 
-    public WrappedPacketOutSetSlot(@NonNull Object handle) {
-        super(handle, Set.of(
-                        version.getPackageNms() + "PacketPlayOutSetSlot",
-                        networkProtocolGame + "PacketPlayOutSetSlot"
-                )
-        );
-    }
+	public WrappedPacketOutSetSlot(@NonNull Object handle) {
+		super(handle, Set.of(
+				version.getPackageNms() + "PacketPlayOutSetSlot",
+				networkProtocolGame + "PacketPlayOutSetSlot"
+			)
+		);
+	}
 }
