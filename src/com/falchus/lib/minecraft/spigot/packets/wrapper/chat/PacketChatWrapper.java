74ca99196d77d4f232b13cf7684b35590acd4f15
Elias <elias@falchus.com>
2026-04-10 22:34:09 +0200

    Update PacketChatWrapper.java

--- src/com/falchus/lib/minecraft/spigot/packets/wrapper/chat/PacketChatWrapper.java
+++ src/com/falchus/lib/minecraft/spigot/packets/wrapper/chat/PacketChatWrapper.java
@@ -1,35 +1,36 @@
 package com.falchus.lib.minecraft.spigot.packets.wrapper.chat;
 
-import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
-import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import com.falchus.lib.minecraft.spigot.packets.wrapper.PacketWrapper;
+
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(makeFinal = true)
 abstract class PacketChatWrapper extends PacketWrapper {
+	
+	Field message;
 
-    Field message;
+	PacketChatWrapper(@NonNull Object handle, @NonNull Set<String> names) {
+		super(handle, names);
+		
+		message = getFirstField(
+			"unsignedContent",
+			"content",
+			"message",
+			"a"
+		);
+	}
 
-    PacketChatWrapper(@NonNull Object handle, @NonNull Set<String> names) {
-        super(handle, names);
-
-        message = getFirstField(
-                "unsignedContent",
-                "content",
-                "message",
-                "a"
-        );
-    }
-
-    /**
-     * In:	{@link String}
-     * Out:	IChatBaseComponent
-     */
-    public Object getMessage() {
-        return getFieldValue(message);
-    }
-
-    public abstract void setMessage(String message);
+	/**
+	 * In:	{@link String}
+	 * Out:	IChatBaseComponent
+	 */
+	public Object getMessage() {
+		return getFieldValue(message);
+	}
+	
+	public abstract void setMessage(String message);
 }
